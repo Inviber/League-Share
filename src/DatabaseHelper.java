38a59f833d1c5f1717d433ebed551178638ffb86
Alexander Bachmann <alexander.d.bachmann@gmail.com>
2020-02-25 21:15:41 -0600

    user db methods added

--- src/DatabaseHelper.java
+++ src/DatabaseHelper.java
@@ -1,12 +1,16 @@
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import static com.mongodb.client.model.Filters.eq;
+import com.mongodb.client.model.Updates;
+
 
 import java.util.ArrayList;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
 
 
@@ -15,6 +19,9 @@ public class DatabaseHelper {
 	private MongoClientURI uri; // (URI) is a string of characters that unambiguously identifies a particular resource.
 	private MongoClient mongoClient;
 	private MongoDatabase database;
+	
+	private static final String USERS = "Users";
+	private static final String LEAGUES = "Leagues";
 	
 	public DatabaseHelper(String uri, String databaseName)
 	{
@@ -49,6 +56,9 @@ public class DatabaseHelper {
 	
 	public void addDocument(String collectionName, Document newDocument)
 	{
+		//example use
+		//dbHelper.addDocument("Users", newUserDocument);
+
 		this.database.getCollection(collectionName).insertOne(newDocument);
 	}
 	
@@ -61,20 +71,107 @@ public class DatabaseHelper {
 	
 	public Document createDocument()
 	{
+		//example use
+		//Document newUserDocument = dbHelper.createDocument();
 		return new Document();
 	}
 	
-	public String createNewUser() 
+	public String createNewUser(String username, String password, String firstName, String lastName) 
 	{
+		//creating document that will be passed into DB
+		Document newUserDocument = new Document();
+		
+		newUserDocument.put("username", username);
+		newUserDocument.put("password", password);
+		newUserDocument.put("firstName", firstName);
+		newUserDocument.put("lastName", lastName);
+		
+		//initializing ID arrays
+		ArrayList<String> followedLeagueIDs = new ArrayList<String>();
+		ArrayList<String> followedTeamIDs = new ArrayList<String>();
+		ArrayList<String> ownedLeagueIDs = new ArrayList<String>();
+		ArrayList<String> ownedTeamIDs = new ArrayList<String>();
+		ArrayList<String> managedTeamIDs = new ArrayList<String>();
+		ArrayList<String> leagueCastedIDs = new ArrayList<String>();
+				
+		newUserDocument.put("followedLeagueIDs", followedLeagueIDs);
+		newUserDocument.put("followedTeamIDs", followedTeamIDs);
+		newUserDocument.put("ownedLeagueIDs", ownedLeagueIDs);
+		newUserDocument.put("ownedTeamIDs", ownedTeamIDs);
+		newUserDocument.put("managedTeamIDs", managedTeamIDs);
+		newUserDocument.put("leagueCastedIDs", leagueCastedIDs);
+		
+		//adding new user to Users collection
+		this.database.getCollection(USERS).insertOne(newUserDocument);
+		
 		//return unique ID
-		return "";
+		return newUserDocument.get("_id").toString();
 	}
 	
+	public void addFollowedLeagueID(String userID, String followedLeagueID)
+	{
+		this.database.getCollection(USERS).updateOne(eq("_id", new ObjectId(userID)), Updates.addToSet("followedLeagueIDs", followedLeagueID));
+	}
 	
+	public void addFollowedTeamID(String userID, String followedTeamID)
+	{
+		this.database.getCollection(USERS).updateOne(eq("_id", new ObjectId(userID)), Updates.addToSet("followedTeamIDs", followedTeamID));
+	}
+	
+	public void addOwnedLeagueID(String userID, String ownedLeagueID)
+	{
+		this.database.getCollection(USERS).updateOne(eq("_id", new ObjectId(userID)), Updates.addToSet("ownedLeagueIDs", ownedLeagueID));
+	}
+	
+	public void addOwnedTeamID(String userID, String ownedTeamID)
+	{
+		this.database.getCollection(USERS).updateOne(eq("_id", new ObjectId(userID)), Updates.addToSet("ownedTeamIDs", ownedTeamID));
+	}
+	
+	public void addManagedTeamID(String userID, String managedTeamID)
+	{
+		this.database.getCollection(USERS).updateOne(eq("_id", new ObjectId(userID)), Updates.addToSet("managedTeamIDs", managedTeamID));
+	}
+	
+	public void addLeagueCastedID(String userID, String leagueCastedID)
+	{
+		this.database.getCollection(USERS).updateOne(eq("_id", new ObjectId(userID)), Updates.addToSet("leagueCastedIDs", leagueCastedID));
+	}
+	
+	public void removeFollowedLeagueID(String userID, String followedLeagueID)
+	{
+		this.database.getCollection(USERS).updateOne(eq("_id", new ObjectId(userID)), Updates.pull("followedLeagueIDs", followedLeagueID));
+	}
+	
+	public void removeFollowedTeamID(String userID, String followedTeamID)
+	{
+		this.database.getCollection(USERS).updateOne(eq("_id", new ObjectId(userID)), Updates.pull("followedTeamIDs", followedTeamID));
+	}
+	
+	public void removeOwnedLeagueID(String userID, String ownedLeagueID)
+	{
+		this.database.getCollection(USERS).updateOne(eq("_id", new ObjectId(userID)), Updates.pull("ownedLeagueIDs", ownedLeagueID));
+	}
+	
+	public void removeOwnedTeamID(String userID, String ownedTeamID)
+	{
+		this.database.getCollection(USERS).updateOne(eq("_id", new ObjectId(userID)), Updates.pull("ownedTeamIDs", ownedTeamID));
+	}
+	
+	public void removeManagedTeamID(String userID, String managedTeamID)
+	{
+		this.database.getCollection(USERS).updateOne(eq("_id", new ObjectId(userID)), Updates.pull("managedTeamIDs", managedTeamID));
+	}
+	
+	public void removeLeagueCastedID(String userID, String leagueCastedID)
+	{
+		this.database.getCollection(USERS).updateOne(eq("_id", new ObjectId(userID)), Updates.pull("leagueCastedIDs", leagueCastedID));
+	}
 	
 	
 	
 
+	
 	public static void main(String[] args)
 	{
 		DatabaseHelper dbHelper = new DatabaseHelper("mongodb+srv://abachmann:mongodb@cluster0-zozah.mongodb.net/test?retryWrites=true&w=majority", "LeagueShare");
@@ -83,10 +180,34 @@ public class DatabaseHelper {
 //		dbHelper.createCollection("Users");
 //		dbHelper.createCollection("Leagues");
 		
+//		MongoCollection<Document> users = dbHelper.getCollection("Users");
+//		MongoCollection<Document> leagues = dbHelper.getCollection("Leagues");
+						
+//		//creating new user and returning the new unique ID
+//		String newUserID = dbHelper.createNewUser("leaf_consumer", "herbivore1993", "Jasper", "Jellington");
+//		
+//		//searching for a specific document by userID
+//		Document searchedDocument = dbHelper.getDocument("Users", newUserID); 
+//		
+//		//printing found document
+//		System.out.println(searchedDocument.toJson());
+	
+			
+		dbHelper.addFollowedLeagueID("5e55dcdf8fe1f34ed9f230ed", "aoeua123eu34098akdsank");
+		dbHelper.addFollowedTeamID("5e55dcdf8fe1f34ed9f230ed", "02934ha123okb");
+		dbHelper.addOwnedLeagueID("5e55dcdf8fe1f34ed9f230ed", "asoenu123thbx90ou70");
+		dbHelper.addOwnedTeamID("5e55dcdf8fe1f34ed9f230ed", "rcg123xbroe98234");
+		dbHelper.addManagedTeamID("5e55dcdf8fe1f34ed9f230ed", "d92123347897oeu00");
+		dbHelper.addLeagueCastedID("5e55dcdf8fe1f34ed9f230ed", "hdm123bngf234871duht");
 		
-		MongoCollection<Document> users = dbHelper.getCollection("Users");
-		MongoCollection<Document> leagues = dbHelper.getCollection("Leagues");
-		
+//		dbHelper.removeFollowedLeagueID("5e55dcdf8fe1f34ed9f230ed", "aoeua123eu34098akdsank");
+//		dbHelper.removeFollowedTeamID("5e55dcdf8fe1f34ed9f230ed", "02934ha123okb");
+//		dbHelper.removeOwnedLeagueID("5e55dcdf8fe1f34ed9f230ed", "asoenu123thbx90ou70");
+//		dbHelper.removeOwnedTeamID("5e55dcdf8fe1f34ed9f230ed", "rcg123xbroe98234");
+//		dbHelper.removeManagedTeamID("5e55dcdf8fe1f34ed9f230ed", "d92123347897oeu00");
+//		dbHelper.removeLeagueCastedID("5e55dcdf8fe1f34ed9f230ed", "hdm123bngf234871duht");
+	
+	
 		/*
 		 * User Document
 		 * 		- _id
@@ -103,7 +224,7 @@ public class DatabaseHelper {
 		 * 		- ownedTeams[] string
 		 * 
 		 * 		- teamsManaged[] string
-		 * 		- leaguesCasted[] string
+		 * 		- leagueCasted[] string
 		 */
 		
 		
@@ -134,48 +255,14 @@ public class DatabaseHelper {
 		
 		
 		
-		//CREATING NEW USER
-		Document newUserDocument = dbHelper.createDocument();
-		
-		newUserDocument.put("username", "grasseater421");
-		newUserDocument.put("password", "eatbeans");
-		newUserDocument.put("firstName", "Tot");
-		newUserDocument.put("lastName", "The Cat");
 		
 		
-		ArrayList<String> followedLeagueIDs = new ArrayList<String>();
-		ArrayList<String> followedTeamIDs = new ArrayList<String>();
-		ArrayList<String> ownedLeagueIDs = new ArrayList<String>();
-		ArrayList<String> ownedTeamIDs = new ArrayList<String>();
-		ArrayList<String> managedTeamIDs = new ArrayList<String>();
-		ArrayList<String> leaguesCastedIDs = new ArrayList<String>();
-		
-		
-		followedLeagueIDs.add("029384oe098hi234");
-		followedLeagueIDs.add("203948aod234oeduh2234");
-		followedLeagueIDs.add("ai7u7e6d9e7u6e7eui43567");
-		
-		
-		newUserDocument.put("followedLeagueIDs", followedLeagueIDs);
-		newUserDocument.put("followedTeamIDs", followedTeamIDs);
-		newUserDocument.put("ownedLeagueIDs", ownedLeagueIDs);
-		newUserDocument.put("ownedTeamIDs", ownedTeamIDs);
-		newUserDocument.put("managedTeamIDs", managedTeamIDs);
-		newUserDocument.put("leaguesCastedIDs", leaguesCastedIDs);
-					
-
-		//adding new user
-//		dbHelper.addDocument("Users", newUserDocument);
-//		//printing new user
-//		System.out.println(newUserDocument.toString());
 //		//example of how to get the value by using the key
 //		System.out.println(newUserDocument.get("username"));
-//		//example of how to get the users unique id
-//		System.out.println(newUserDocument.get("_id"));
+//		//example of how to get the users unique id - will be null unless added to or is in DB 
+//		System.out.println(newUserDocument.get("_id").toString());
+			
 		
-		Document searchedDocument = dbHelper.getDocument("Users", "5e558fa2db3ad400422015af"); 
-		
-		System.out.println(searchedDocument.toJson());
 	
 		//shutting down mongoDB connection
 		dbHelper.getClient().close();
