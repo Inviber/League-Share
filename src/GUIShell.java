0df26d28825483613b46955ccb366d0a5ea09144
Alexander Bachmann <alexander.d.bachmann@gmail.com>
2020-03-15 15:52:33 -0500

    application now opens in center of screen

--- src/GUIShell.java
+++ src/GUIShell.java
@@ -1,6 +1,10 @@
+
+
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Monitor;
 import org.eclipse.swt.widgets.Shell;
 
 public class GUIShell extends Shell {
@@ -27,6 +31,15 @@ public class GUIShell extends Shell {
 		try {
 			Display display = Display.getDefault();
 			GUIShell shell = new GUIShell(display);
+			
+			//Centering application when it opens
+			Monitor primary = display.getPrimaryMonitor();
+			Rectangle bounds = primary.getBounds();
+			Rectangle rect = shell.getBounds();
+			int x = bounds.x + (bounds.width - rect.width) / 2;
+			int y = bounds.y + (bounds.height - rect.height) / 2;
+			shell.setLocation(x, y);
+			
 			shell.open();
 			shell.layout();
 			while (!shell.isDisposed()) {			
