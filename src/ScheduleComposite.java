e835c34b26dd9c9ecfc5b62e581b7ded2f7a19bc
Luke <law67@students.uwf.edu>
2020-03-17 18:39:18 -0500

    ScheduleComposite now presents dynamic match details based on date.

--- src/ScheduleComposite.java
+++ src/ScheduleComposite.java
@@ -2,7 +2,10 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 import org.eclipse.swt.SWT;
@@ -25,11 +28,16 @@ public class ScheduleComposite extends Composite {
 	 * @param parent
 	 * @param style
 	 */
+	@SuppressWarnings("deprecation")
 	public ScheduleComposite(Composite parent, int style, GUIShell shell, DatabaseHelper dbHelper, LeagueParser leagueParser) {
 		super(parent, style);
 		
 		Color dark_gray = getDisplay().getSystemColor(SWT.COLOR_DARK_GRAY);
-//		Date today = new Date();
+		Calendar today = Calendar.getInstance();
+		Calendar matchCal = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yy");
+		Date matchDate = new Date();
+		
 		
 		System.out.println("schedule made");
 		setLayout(new FormLayout());
@@ -90,33 +98,63 @@ public class ScheduleComposite extends Composite {
 			TeamParser team1 = new TeamParser(leagueParser.getLeagueID(), match1.getHomeTeamID(), dbHelper);
 			TeamParser team2 = new TeamParser(leagueParser.getLeagueID(), match1.getAwayTeamID(), dbHelper);
 			
-			Composite composite = new Composite(grpMatches, SWT.NONE);
+			Composite matchComp = new Composite(grpMatches, SWT.NONE);
 			GridData gd_composite = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
 			gd_composite.widthHint = 305;
 			gd_composite.heightHint = 201;
-			composite.setLayoutData(gd_composite);
+			matchComp.setLayoutData(gd_composite);
 
-			Label lblMatchDate = new Label(composite, SWT.NONE);
+			Label lblMatchDate = new Label(matchComp, SWT.NONE);
 			lblMatchDate.setBounds(10, 23, 248, 15);
 			lblMatchDate.setText(match1.getDate());
 			lblMatchDate.setAlignment(SWT.CENTER);
 
-			Label lblTeam1 = new Label(composite, SWT.NONE);
+			Label lblTeam1 = new Label(matchComp, SWT.NONE);
 			lblTeam1.setText(team1.getTeamName());
 			lblTeam1.setAlignment(SWT.CENTER);
 			lblTeam1.setBounds(10, 60, 248, 15);
 
-			Label lblTeam2 = new Label(composite, SWT.NONE);
+			Label lblTeam2 = new Label(matchComp, SWT.NONE);
 			lblTeam2.setText(team2.getTeamName());
 			lblTeam2.setAlignment(SWT.CENTER);
 			lblTeam2.setBounds(10, 153, 248, 15);
 
-			Label lblVs = new Label(composite, SWT.NONE);
+			Label lblVs = new Label(matchComp, SWT.NONE);
 			lblVs.setText("V.S.");
 			lblVs.setAlignment(SWT.CENTER);
 			lblVs.setBounds(102, 104, 55, 15);
 			
-			
+			try {
+				matchDate = sdf.parse( match1.getDate() );
+			} catch (ParseException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+//			System.out.println(matchDate);
+			matchCal.setTime(matchDate);
+	        
+	        if( matchCal.before(today) ) {
+	        	// Print final match score to matchComp
+//	        	System.out.println("Before Today.");
+				lblTeam1.setText( team1.getTeamName() + " | " + match1.getHomeScore() );
+				lblTeam2.setText( team2.getTeamName() + " | " + match1.getAwayScore() );
+	        	
+	        } else if( matchCal.equals(today) ) {
+	        	// Present 'Spectate' button on matchComp
+//	        	System.out.println("Today.");
+				
+//				Button spectateMatch = new Button(matchComp, SWT.NONE);
+//				spectateMatch.addSelectionListener(new SelectionAdapter() {
+//					@Override
+//					public void widgetSelected(SelectionEvent e) {
+////						shell.disposeDisplayedComposite();
+//						SpectateComposite spectateComposite = new SpectateComposite(shell, SWT.NONE, shell, dbHelper, match1);
+//						shell.setDisplayedComposite(spectateComposite);
+//					}
+//				});
+//				spectateMatch.setText("SPECTATE");
+	        	
+	        }
 
 		}
 	}
