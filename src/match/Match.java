36a0c7d347b9d5c13662aed55a335bba40ad5c18
Luke <law67@students.uwf.edu>
2020-03-24 14:39:50 -0500

    Match pojo created.

---
+++ src/match/Match.java
@@ -0,0 +1,56 @@
+package match;
+
+import java.util.Calendar;
+
+public class Match {
+	// private String leagueID;
+	// private String matchID;
+	private String homeTeamID;
+	private String awayTeamID;
+	private int homeScore;
+	private int awayScore;
+    private Calendar date;
+    
+    public Match() {}
+
+    public Match(String homeTeamID, String awayTeamID, int homeScore, int awayScore, Calendar date) {
+        this.homeTeamID = homeTeamID;
+        this.awayTeamID = awayTeamID;
+        this.homeScore = homeScore;
+        this.awayScore = awayScore;
+        this.date = date;
+    }
+
+    public String getHomeTeamID() {
+        return homeTeamID;
+    }
+
+    public String getAwayTeamID() {
+        return awayTeamID;
+    }
+
+    public int getHomeScore() {
+        return homeScore;
+    }
+
+    public int getAwayScore() {
+        return awayScore;
+    }
+
+    public Calendar getDate() {
+        return date;
+    }
+
+    public void setHomeScore(int homeScore) {
+        this.homeScore = homeScore;
+    }
+
+    public void setAwayScore(int awayScore) {
+        this.awayScore = awayScore;
+    }
+
+    public void setDate(Calendar date) {
+        this.date = date;
+    }
+
+}
