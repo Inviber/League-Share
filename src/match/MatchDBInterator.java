c7d8099ee0c1e05699e630c09df34dedd6031c54
Luke <law67@students.uwf.edu>
2020-03-26 10:02:44 -0500

    MatchDBInterator created.

---
+++ src/match/MatchDBInterator.java
@@ -0,0 +1,50 @@
+package match;
+
+import org.bson.Document;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import database.DatabaseHelper;
+
+public class MatchDBInterator {
+	
+	private JSONObject matchData;
+	private JSONParser parser = new JSONParser();
+	private DatabaseHelper dbHelper;
+	
+	public MatchDBInterator(DatabaseHelper dbHelper) {
+		this.dbHelper = dbHelper;
+	}
+	
+	public JSONObject getMatchDetails(String leagueID, String matchID) {
+		Document teamDocument = dbHelper.getMatchDocumentByID(leagueID, matchID);
+
+		try 
+		{
+			Object obj = parser.parse(teamDocument.toJson());
+			JSONObject leagueData = (JSONObject) obj;
+			
+			JSONArray matchDataArray = (JSONArray) leagueData.get("matches");
+									
+			for (int i = 0; i < matchDataArray.size(); i++)
+			{
+				JSONObject currentMatchData = (JSONObject) matchDataArray.get(i);
+				String oid = currentMatchData.get("_id").toString(); 
+				String[] id = oid.split("\""); // removing oid from string.
+				if (id[3].equals(matchID)) // if this is the id searched for...
+				{
+					matchData = currentMatchData; // save this data.
+					break;
+				}
+			}
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		
+		return matchData;
+	}
+	
+}
