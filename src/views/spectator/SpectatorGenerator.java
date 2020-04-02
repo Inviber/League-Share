711296e504d49771d4727b145bee478eeac001e3
Joshua Kleine <jdk31@students.uwf.edu>
2020-04-02 14:13:05 -0500

    Changed spectator generator to work like schedule generator

--- src/views/spectator/SpectatorGenerator.java
+++ src/views/spectator/SpectatorGenerator.java
@@ -10,16 +10,17 @@ import views.GUIShell;
 public class SpectatorGenerator {
 	
 	
-	public SpectatorGenerator()
+	public SpectatorGenerator(Composite parent, int style, GUIShell shell, Match match, Team homeTeam, Team awayTeam, Composite previousWindow)
 	{
+		
+		shell.setDisplayedComposite(generateSpectator( parent,  style,  shell,  match,  homeTeam,  awayTeam,  previousWindow));
 	
 	}
 	
-	public SpectatorComposite generateSpectator(Composite parent, int style, GUIShell shell, Match match, Team homeTeam, Team awayTeam, Composite previousWindow)
+	public SpectatorComposite  generateSpectator(Composite parent, int style, GUIShell shell, Match match, Team homeTeam, Team awayTeam, Composite previousWindow)
 	
 	{
 		SpectatorComposite spectatorComposite = new SpectatorComposite(parent, style, shell, match, homeTeam, awayTeam, previousWindow );
-		  
 		  return spectatorComposite;
 	}
 }
