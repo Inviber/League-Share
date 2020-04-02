1efcdfc9e7eb7db6595e30b319a49fd7da47fa28
Joshua Kleine <jdk31@students.uwf.edu>
2020-04-02 13:29:37 -0500

    SpectatorGenerator

---
+++ src/views/spectator/SpectatorGenerator.java
@@ -0,0 +1,25 @@
+package views.spectator;
+
+import org.eclipse.swt.widgets.Composite;
+
+import match.Match;
+import team.Team;
+import views.GUIShell;
+
+
+public class SpectatorGenerator {
+	
+	
+	public SpectatorGenerator()
+	{
+	
+	}
+	
+	public SpectatorComposite generateSpectator(Composite parent, int style, GUIShell shell, Match match, Team homeTeam, Team awayTeam, Composite previousWindow)
+	
+	{
+		SpectatorComposite spectatorComposite = new SpectatorComposite(parent, style, shell, match, homeTeam, awayTeam, previousWindow );
+		  
+		  return spectatorComposite;
+	}
+}
