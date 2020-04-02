efd6b937f498991b9d73440fc746b23c127999c1
Joshua Kleine <jdk31@students.uwf.edu>
2020-04-01 22:44:16 -0500

    Spectator Completely Refactored into methods

--- src/views/spectator/SpectatorComposite.java
+++ src/views/spectator/SpectatorComposite.java
@@ -35,13 +35,59 @@ public class SpectatorComposite extends Composite {
 	 * 
 	 * @param homeTeam Home team parameter should be before away team
 	 * @param awayTeam 
-	 * 
+	 * @param previousWindow - the Schedule composite should pass "this" keyword as the parameter
 	 * Called using the POJOs from Schedule, can make generators to go back to the schedule
 	 */
 	public SpectatorComposite(Composite parent, int style, GUIShell shell, Match match,
-			Team homeTeam, Team awayTeam, SpectatorComposite previousWindow) {
+			Team homeTeam, Team awayTeam, Composite previousWindow) {
 		super(parent, style);
 
+		CreateComponents(shell, parent, match, homeTeam, awayTeam, previousWindow);
+		
+	}
+	
+	private void CreateComponents(GUIShell shell, Composite parent, Match match,
+			Team homeTeam, Team awayTeam, Composite previousWindow) {
+		//TODO make a caster composite and replace this blank one;
+		Composite temporaryCaster = new Composite(parent, SWT.NONE);
+		CreateTopButtons(previousWindow, temporaryCaster, parent);
+		CreateChat();
+		CreateConstantLabels();
+		CreateDynamicDataLabels(match, homeTeam, awayTeam, shell);
+		
+	}
+	
+	
+	private void CreateTopButtons(Composite previousWindow, Composite CasterComposite, Composite parent){
+	
+	//button to switch to caster (no caster composite for now)
+		Button switchToCaster = new Button(this, SWT.NONE);
+		switchToCaster.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				((GUIShell) parent).setDisplayedComposite(CasterComposite);
+				System.out.println("Caster Selected");
+			}
+		});
+		switchToCaster.setBounds(10, 61, 134, 45);
+		switchToCaster.setText("Caster View");
+		
+	//button to go back to schedule
+		Button backButton = new Button(this, SWT.NONE);
+		backButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				((GUIShell) parent).setDisplayedComposite(previousWindow);
+				System.out.println("Back button pressed.");
+			}
+		});
+		backButton.setText("Back");
+		backButton.setBounds(10, 10, 134, 45);
+		
+		
+	}
+	
+	private void CreateConstantLabels() {
 		Label lblNewLabel = new Label(this, SWT.NONE);
 		lblNewLabel.setFont(SWTResourceManager.getFont("Segoe UI", 12, SWT.NORMAL));
 		lblNewLabel.setAlignment(SWT.CENTER);
@@ -53,7 +99,12 @@ public class SpectatorComposite extends Composite {
 		lblNewLabel_1.setFont(SWTResourceManager.getFont("Segoe UI", 9, SWT.NORMAL));
 		lblNewLabel_1.setBounds(628, 80, 24, 20);
 		lblNewLabel_1.setText("vs.");
-
+		
+		
+	}
+	
+	//Player Lists could be done more eloquently
+	private void CreateDynamicDataLabels(Match match, Team homeTeam, Team awayTeam, GUIShell shell) {
 		Label lblNewLabel_2 = new Label(this, SWT.NONE);
 		lblNewLabel_2.setAlignment(SWT.CENTER);
 		lblNewLabel_2.setBounds(320, 80, 306, 20);
@@ -68,46 +119,7 @@ public class SpectatorComposite extends Composite {
 		lblNewLabel_3.setAlignment(SWT.CENTER);
 		lblNewLabel_3.setBounds(427, 125, 431, 20);
 		lblNewLabel_3.setText(match.getHomeScore() + "   -   " + match.getAwayScore());
-
-		Group grpChatToBe = new Group(this, SWT.NONE);
-		grpChatToBe.setText("Chat to be completed");
-		grpChatToBe.setBounds(10, 466, 1260, 219);
-
-		ScrolledComposite scrolledComposite = new ScrolledComposite(grpChatToBe,
-				SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
-		scrolledComposite.setBounds(0, 21, 1260, 163);
-		scrolledComposite.setExpandVertical(true);
-
-		Button btnSubmit = new Button(grpChatToBe, SWT.NONE);
-		btnSubmit.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-			}
-		});
-
-		btnSubmit.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyPressed(KeyEvent e) {
-				if (e.keyCode == SWT.CR) {
-					System.out.println("Message Sent");
-					// Need to actually send message here using the text field contents [Should be
-					// the same as above]
-				}
-			}
-		});
-		btnSubmit.setBounds(428, 183, 90, 26);
-		btnSubmit.setText("Submit");
-
-		txtEnterAMessage = new Text(grpChatToBe, SWT.BORDER);
-		txtEnterAMessage.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				txtEnterAMessage.setText("");
-			}
-		});
-		txtEnterAMessage.setBounds(0, 184, 428, 25);
-		txtEnterAMessage.setText("Enter a message");
-
+		
 		Group grpHomeTeamInfo = new Group(this, SWT.NONE);
 		grpHomeTeamInfo.setText("Home Team Info");
 		grpHomeTeamInfo.setBounds(20, 151, 606, 313);
@@ -168,26 +180,46 @@ public class SpectatorComposite extends Composite {
 		awayTeamPlayersComposite.setContent(composite);
 		awayTeamPlayersComposite.setMinSize(composite.computeSize(SWT.DEFAULT, SWT.DEFAULT));
 
-		Button btnNewButton = new Button(this, SWT.NONE);
-		btnNewButton.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				// Go to caster Composite
-			}
-		});
-		btnNewButton.setBounds(10, 61, 134, 45);
-		btnNewButton.setText("Caster View");
-		
-		Button btnBack = new Button(this, SWT.NONE);
-		btnBack.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				((GUIShell) parent).setDisplayedComposite(previousWindow);
-				System.out.println("Back button pressed.");
-			}
-		});
-		btnBack.setText("Back");
-		btnBack.setBounds(10, 10, 134, 45);
+	}
+	
+	private void CreateChat() {
+		Group grpChatToBe = new Group(this, SWT.NONE);
+		grpChatToBe.setText("Chat to be completed");
+		grpChatToBe.setBounds(10, 466, 1260, 219);
 
+		ScrolledComposite scrolledComposite = new ScrolledComposite(grpChatToBe,
+				SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+		scrolledComposite.setBounds(0, 21, 1260, 163);
+		scrolledComposite.setExpandVertical(true);
+
+		Button btnSubmit = new Button(grpChatToBe, SWT.NONE);
+		btnSubmit.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
+
+		btnSubmit.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.keyCode == SWT.CR) {
+					System.out.println("Message Sent");
+					// Need to actually send message here using the text field contents [Should be
+					// the same as above]
+				}
+			}
+		});
+		btnSubmit.setBounds(428, 183, 90, 26);
+		btnSubmit.setText("Submit");
+
+		txtEnterAMessage = new Text(grpChatToBe, SWT.BORDER);
+		txtEnterAMessage.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				txtEnterAMessage.setText("");
+			}
+		});
+		txtEnterAMessage.setBounds(0, 184, 428, 25);
+		txtEnterAMessage.setText("Enter a message");
 	}
 }
