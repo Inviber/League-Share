909bf4124ab8f98907b5f16568d54de22cddfd6c
Joshua LaRocca <42752698+FakeTails@users.noreply.github.com>
2020-04-08 14:30:17 -0500

    Updated PlayerDBInteractor and interface to reflect new functionality of updating stats.

--- src/player/PlayerDBInteratorInterface.java
+++ src/player/PlayerDBInteratorInterface.java
@@ -5,4 +5,5 @@ import org.json.simple.JSONObject;
 public interface PlayerDBInteratorInterface {
 
 	public JSONObject getPlayerDetails(String leagueID, String teamID, String playerID, boolean print);
+	public void updatePlayerStatistics(String leagueID, String teamID, String playerID, String statName, int statValue);
 }
